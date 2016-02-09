2f68b0fda4f7366bd3f42c2ac7163b648b1f88c6
Alex <amhughes@g.cofc.edu>
2016-02-09 11:09:49 -0500

    Create animal classes

---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+/**
+ * Created by alexanderhughes on 2/9/16.
+ */
+public class Dog extends Mammal {
+
+    public Dog() {
+        this.name = "Dog";
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Bark!");
+    }
+}
